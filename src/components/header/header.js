336d6c14098456edc3abf3c29d464a878b0cf6ca
linqiyuan <466214222@qq.com>
2020-05-29 10:45:16 +0800

    修复顶部头部导航栏样式问题

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -12,6 +12,8 @@ class Header extends Component {
 						<img alt="leftIcon" src={leftIcon} className="header__LeftIcon-img"/>
 					</div>
 					<span className="header__Title">{this.props.headerTitle}</span>
+					<div className="header__Right">
+					</div>
 				</div>
 			</div>
 		);
