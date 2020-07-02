3c3cdbd5cf35411d7a365ae7ad3f5a01281d7613
linqiyuan <466214222@qq.com>
2020-07-02 15:12:43 +0800

    抽取组件，完善左边导航

--- src/pages/home/homeIndex.js
+++ src/pages/home/homeIndex.js
@@ -16,17 +16,25 @@ class HomeIndex extends Component {
 	}
 
 	handleClick = e => {
-		console.log(e);
+		console.log(e.key)
+		switch (e.key) {
+			case '1':
+				this.props.history.push("/homeIndex/workExprience");
+				break;
+			case '2':
+				this.props.history.push("/");
+				break;
+		}
 	};
 
 	render() {
 		return (
 			<div className="homeIndex">
 				{/*左边菜单*/}
-				<div className="lefMenu">
+				<div className="leftMenu">
 					<Menu
 						onClick={this.handleClick}
-						style={{width: 256 , height:"100%"}}
+						style={{width: 256, height: "100%"}}
 						defaultSelectedKeys={['1']}
 						defaultOpenKeys={['sub1']}
 						mode="inline"
@@ -34,16 +42,16 @@ class HomeIndex extends Component {
 						<SubMenu
 							key="sub1"
 							title={
-								<span>
-				              <span>个人简历</span>
+							<span className="nes-pointer">
+				              <span >个人简历</span>
 				            </span>}>
 							<Menu.ItemGroup key="g1" title="经历方面">
-								<Menu.Item key="1">工作经历</Menu.Item>
-								<Menu.Item key="2">项目经验</Menu.Item>
+								<Menu.Item key="1" className="nes-pointer">工作经历</Menu.Item>
+								<Menu.Item key="2" className="nes-pointer">项目经验</Menu.Item>
 							</Menu.ItemGroup>
 							<Menu.ItemGroup key="g2" title="个人方面">
-								<Menu.Item key="3">个人能力</Menu.Item>
-								<Menu.Item key="4">个人荣誉</Menu.Item>
+								<Menu.Item key="3" className="nes-pointer">个人能力</Menu.Item>
+								<Menu.Item key="4" className="nes-pointer">个人荣誉</Menu.Item>
 							</Menu.ItemGroup>
 						</SubMenu>
 					</Menu>
--- src/pages/workExprience/workExprience.js
+++ src/pages/workExprience/workExprience.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './personalPage.scss'
+import './workExprience.scss'
 
 import { Input } from 'antd';
 import ContentCard from "../../components/contentCard/contentCard";
@@ -13,9 +13,6 @@ class WorkExprience extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			inputValue: "",
-			workAbility: ["熟练...", "熟练...", "熟练...", "熟练...", "熟悉..."],
-			itemExperience: ["广州天高软件科技公司官网"],
 			workCompanies: ["广州天高软件科技有限公司", "广州明动软件股份有限公司"],
 		}
 	}
@@ -36,26 +33,8 @@ class WorkExprience extends Component {
 					<div>
 						<ContentCard>
 							<div>
-								{"个人能力"}
-								{this.state.workAbility}
-							</div>
-						</ContentCard>
-						<ContentCard>
-							<div>
-								{"项目经验"}
-								{this.state.itemExperience}
-							</div>
-						</ContentCard>
-						<ContentCard>
-							<div>
 								{"工作经历"}
 								{this.state.workCompanies}
-							</div>
-						</ContentCard>
-						<ContentCard>
-							<div>
-								{"个人简介"}
-								{"主业是音乐、健身，副业兼职前端开发的一个人下人"}
 							</div>
 						</ContentCard>
 					</div>
