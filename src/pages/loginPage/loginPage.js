0736def7e50cfc7082176e2be3a9edc53a46d2a5
linqiyuan <466214222@qq.com>
2020-07-10 10:52:46 +0800

    整合Ant Motion 至开始页面（退场动画）

--- src/pages/loginPage/loginPage.js
+++ src/pages/loginPage/loginPage.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
-import {message} from 'antd';
 import './loginPage.scss';
-import PixDialog from "../../components/pixDialog/pixDialog";
+import RcQueueAnim from "rc-queue-anim";
 
 
 class LoginPage extends Component {
@@ -14,70 +13,29 @@ class LoginPage extends Component {
 				password: ""
 			},
 			showDialog: false,
-			pageAnimation:""
+			pageShow: true,
+			setTime: ""
 		}
 	}
 
 	componentDidMount() {
-		this.setState({
-			pageAnimation:setTimeout(()=>{
-				this.props.history.push("/homeIndex/workExprience")
-			},3600)
-		})
 	}
 
 	componentWillUnmount() {
-		//清空setTime
 		this.setState({
-			pageAnimation:""
+			setTime: ""
 		})
 	}
 
 
 	//Methods============
-	//登录
-	login() {
-		console.log(this.state.formValue.userName, this.state.formValue.password)
-		if (this.state.formValue.userName === 'linqy' && this.state.formValue.password === '131898') {
-			message.success("登录成功~！");
-			this.props.history.push("/homeIndex/workExprience")
-		} else {
-			message.warning("请检查账号密码是否正确~！");
-		}
-	}
 
-	//注册
-	register() {
-		this.changeFlag(true);
-	}
-
-	//改变弹窗显示
-	changeFlag(flagVal) {
-		console.log(flagVal)
+	handleClick() {
 		this.setState({
-			showDialog: flagVal,
-		})
-	}
-
-	//显示弹窗
-	showDialogFlag() {
-		const showValue = {
-			tips: "提示！",
-			content: "暂时不提供注册哦~！"
-		}
-		return (
-			this.state.showDialog ? (
-				<PixDialog changeFlag={(flagVal) => this.changeFlag(flagVal)} showValue={showValue}/>
-			) : null
-		)
-	}
-
-	handleChange(e) {
-		this.setState({
-			formValue: {
-				...this.state.formValue,
-				[e.target.name]: e.target.value,
-			}
+			pageShow: !this.state.pageShow,
+			setTime: setTimeout(() => {
+				this.props.history.push("/homeIndex/workExprience")
+			}, 1100)
 		})
 	}
 
@@ -88,38 +46,35 @@ class LoginPage extends Component {
 	render() {
 		return (
 			//登录主页
-			<div className="loginPage">
-				{/*登录表单*/}
-				<div className="loginForm">
-					<h1 className="titleLogo">Sev3n像素风</h1>
-					{/*/!*像素表单*!/*/}
-					{/*/!*用户名*!/*/}
-					{/*<div className="nes-field formItem">*/}
-					{/*	<label htmlFor="name_field">用户名</label>*/}
-					{/*	<input type="text" id="name_field" className="nes-input" name="userName"*/}
-					{/*	       onChange={(e) => this.handleChange(e)}/>*/}
-					{/*</div>*/}
-					{/*/!*密码*!/*/}
-					{/*<div className="nes-field formItem">*/}
-					{/*	<label htmlFor="name_field">密码</label>*/}
-					{/*	<input type="password" id="pwd_field" className="nes-input" name="password"*/}
-					{/*	       onChange={(e) => this.handleChange(e)}/>*/}
-					{/*</div>*/}
+			//动画设置
+			<RcQueueAnim duration={1000} type={'top'} appear={false} key="demo"
+			             ease={'easeInOutQuart'}
+			             animConfig={[
+				             {opacity: [1, 0], translateY: [0, '-100%']}
+			             ]}
+			>
+				{this.state.pageShow ? [
+					<div className="loginPage" key="animateA" onClick={() => this.handleClick()}>
+						{/*登录表单*/}
+						<div className="loginForm">
+							<h1 className="titleLogo">Sev3n像素风</h1>
+							<h3 className="titleLogo">林启元的个人简历</h3>
+						</div>
 
-					{/*<button type="button" className="nes-btn is-primary mr20" onClick={() => this.login()}>登录</button>*/}
-					{/*<button type="button" className="nes-btn" onClick={() => this.register()}>注册</button>*/}
-					<h3 className="titleLogo">林启元的个人简历</h3>
-				</div>
-				{/*弹窗显示*/}
-				{this.showDialogFlag()}
+						<div className="mouseClick">
+							<span>鼠标点击进入</span>
+						</div>
 
-				{/*nes图标*/}
-				<div className="fixBottom">
-					<i className="nes-pokeball fl"></i>
-					<i className="nes-kirby fr"></i>
-					<div className="clearFix"></div>
-				</div>
-			</div>
+						{/*nes图标*/}
+						<div className="fixBottom">
+							<i className="nes-pokeball fl"></i>
+							<i className="nes-kirby fr"></i>
+							<div className="clearFix"></div>
+						</div>
+					</div>
+				] : null}
+
+			</RcQueueAnim>
 		);
 	}
 }
